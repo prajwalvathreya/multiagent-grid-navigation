43b4c0dc7e417b8b1c3212fb16c8b41588385c67
Akshaj Aradhya <56526237+AkshajAradhya@users.noreply.github.com>
2023-11-25 04:35:16 -0500

    Update sarsa.py

--- sarsa.py
+++ sarsa.py
@@ -1,23 +1,20 @@
-
 import numpy as np
-
-# Define the grid world
-GRID_SIZE = 5
-START_STATE = (0, 0)
-GOAL_STATE = (GRID_SIZE-1, GRID_SIZE-1)
-OBSTACLES = [(1, 1), (2, 2), (3, 3)]  # You can customize the obstacle positions
-
-# Define possible actions (up, down, left, right)
-ACTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-NUM_ACTIONS = len(ACTIONS)
+import random
+import pygame
+from generate_maze import *
+from maze_environment import *
 
 # Define SARSA parameters
 ALPHA = 0.1  # learning rate
 GAMMA = 0.9  # discount factor
 EPSILON = 0.1  # exploration-exploitation trade-off
 
+# Define possible actions (up, down, left, right)
+ACTIONS = [0, 1, 2, 3]
+NUM_ACTIONS = len(ACTIONS)
+
 # Initialize Q-values
-Q = np.zeros((GRID_SIZE, GRID_SIZE, NUM_ACTIONS))
+Q = np.zeros((16, 16, NUM_ACTIONS))
 
 # Function to choose an action using epsilon-greedy policy
 def choose_action(state):
@@ -28,41 +25,44 @@ def choose_action(state):
 
 # Function to perform SARSA update
 def sarsa_update(state, action, reward, next_state, next_action):
-    current_value = Q[state][action]
-    next_value = Q[next_state][next_action]
+    current_value = Q[state[0]][state[1]][action]
+    next_value = Q[next_state[0]][next_state[1]][next_action]
     new_value = current_value + ALPHA * (reward + GAMMA * next_value - current_value)
-    Q[state][action] = new_value
+    Q[state[0]][state[1]][action] = new_value
 
 # Function to simulate one episode in the grid world
-def run_episode():
+def run_episode(env):
     total_reward = 0
-    current_state = START_STATE
-    current_action = choose_action(current_state)
+    state = env.reset()
 
-    while current_state != GOAL_STATE:
-        next_state = (current_state[0] + ACTIONS[current_action][0], current_state[1] + ACTIONS[current_action][1])
+    while True:
+        actions = [choose_action(state[i]) for i in range(env.n_agents)]
+        next_states, rewards = env.step(actions)
 
-        # Check if the next state is valid (not outside the grid or an obstacle)
-        if 0 <= next_state[0] < GRID_SIZE and 0 <= next_state[1] < GRID_SIZE and next_state not in OBSTACLES:
-            # Calculate reward (negative for each step until reaching the goal)
-            reward = -1
-            if next_state == GOAL_STATE:
-                reward = 0
-
+        for i in range(env.n_agents):
+            next_state = next_states[i]
+            reward = rewards[i]
             next_action = choose_action(next_state)
-            sarsa_update(current_state, current_action, reward, next_state, next_action)
 
-            current_state = next_state
-            current_action = next_action
+            sarsa_update(state[i], actions[i], reward, next_state, next_action)
+
+            state[i] = next_state
             total_reward += reward
 
-    return total_reward
+            if all(state[i] == env.goal_position for i in range(env.n_agents)):
+                return total_reward
+
+# Create the maze environment
+maze, start_row, start_col, end_row, end_col = generate_maze()
+start_positions = [(start_row, start_col)] * 3  # Assume 3 agents for simplicity
+goal_position = (end_row, end_col)
+env = MazeEnvironment(maze, start_positions, goal_position)
 
 # Train the agent
 NUM_EPISODES = 1000
 
 for episode in range(NUM_EPISODES):
-    total_reward = run_episode()
+    total_reward = run_episode(env)
 
     if episode % 100 == 0:
         print(f"Episode {episode}, Total Reward: {total_reward}")
