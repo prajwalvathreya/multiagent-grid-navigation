ad217ede54c2547e73904e0876627386ca096f0a
sougata1996 <sougatamukherjeejsr@gmail.com>
2023-11-30 16:16:46 -0500

    Making some changes.

--- single_agent_sarsa.py
+++ single_agent_sarsa.py
@@ -1,69 +1,71 @@
-# Importing Libraries
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
 
-# Set the learning rate, discount factor, and number of episodes (hardcoded values)
-lr = 0.8
-gemma = 0.95
+# SARSA parameters
+lr = 0.1
+gamma = 0.99
+epsilon = 0.1
 num_episodes = 10000
 
-# Create the environment
-env = gym.make('FrozenLake-v1')
-print(env)
+# Create the FrozenLake environment
+env = gym.make('FrozenLake8x8-v1')
 
 # Initialize the Q-table
-Q = np.random.uniform(low=0, high=0.01, size=(
-    env.observation_space.n, env.action_space.n))
+Q = np.zeros((env.observation_space.n, env.action_space.n))
 
-# lists to store performance metrics
+# Lists to store performance metrics
 total_rewards = []
 total_steps = []
 success_rate = []
-test_reward = []
 
-# Run the SARSA algorithm
-for i in range(num_episodes):
-    s, info = env.reset()
+# SARSA algorithm
+for episode in range(num_episodes):
+    state, info = env.reset()
+    action = env.action_space.sample() if np.random.rand(
+    ) < epsilon else np.argmax(Q[state])
+
     done = False
     episode_reward = 0
-    num_steps = 0
+    episode_steps = 0
 
-    # Choose the first action using epsilon-greedy policy
-    a = np.argmax(Q[s, :] + np.random.randn(1,
-                  env.action_space.n)[0] * (1. / (i + 1)))
-
-    # The SARSA learning algorithm
     while not done:
-        s_, r, done, _, info = env.step(a)
+        next_state, reward, done, _, info = env.step(action)
+        next_action = env.action_space.sample() if np.random.rand(
+        ) < epsilon else np.argmax(Q[next_state])
 
-        # Choose the next action using epsilon-greedy policy
-        a_ = np.argmax(Q[s_, :] + np.random.randn(1,
-                       env.action_space.n)[0] * (1. / (i + 1)))
+        # SARSA update
+        Q[state, action] = Q[state, action] + lr * \
+            (reward + gamma * Q[next_state, next_action] - Q[state, action])
 
-        # update Q values using SARSA update rule
-        Q[s, a] = Q[s, a] + lr * (r + gemma * Q[s_, a_] - Q[s, a])
+        state = next_state
+        action = next_action
+        episode_reward += reward
+        episode_steps += 1
 
-        s = s_
-        a = a_
-        episode_reward += r
-        num_steps += 1
-
-    # append performance metrics to lists
-    total_rewards.append(episode_reward / num_steps)
-    total_steps.append(num_steps)
+    total_rewards.append(episode_reward)
+    total_steps.append(episode_steps)
     success_rate.append(int(episode_reward > 0))
 
-    # Print episode metrics
-    print("Episode:", i + 1, "Reward:", episode_reward, "Steps:", num_steps)
+    print(
+        f"Episode: {episode}, Total Reward: {episode_reward}, Total Steps: {episode_steps}")
 
-# Plotting the average rewards and episode Lengths gained throughout each episode per episode
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-axs[0].plot(total_rewards, 'tab:green')
-axs[0].set_title('Average Reward per Episode')
-axs[1].plot(total_steps, 'tab:purple')
-axs[1].set_title('Number of Steps Taken per Episode')
+# Plotting total rewards
+plt.figure(figsize=(10, 5))
+plt.plot(total_rewards, label='Total Rewards', color='tab:blue')
+plt.title('Total Rewards per Episode')
+plt.xlabel('Episode')
+plt.ylabel('Total Reward')
+plt.legend()
+plt.show()
 
+# Plotting total steps
+plt.figure(figsize=(10, 5))
+plt.plot(total_steps, label='Total Steps', color='tab:orange')
+plt.title('Total Steps per Episode')
+plt.xlabel('Episode')
+plt.ylabel('Total Steps')
+plt.legend()
 plt.show()
 
 # Print overall metrics
