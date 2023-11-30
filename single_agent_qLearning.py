8d515e8e819e03df07333f721b4d200171e1d2ea
sougata1996 <sougatamukherjeejsr@gmail.com>
2023-11-30 14:54:16 -0500

    Fix Q Learning

--- single_agent_qLearning.py
+++ single_agent_qLearning.py
@@ -1,12 +1,17 @@
 # Importing Libraries
+import random
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
 
 # Set the learning rate, discount factor, and number of episodes (hardcoded values)
-lr = 0.8
-gemma = 0.95
+lr = 1
+gamma = 0.9
 num_episodes = 10000
+
+# Epsilon parameters
+epsilon = 0.9
+epsilon_decay = 0.999  # You can adjust the decay rate
 
 # Create the environment
 env = gym.make('FrozenLake-v1')
@@ -15,7 +20,7 @@ env = gym.make('FrozenLake-v1')
 Q = np.random.uniform(low=0, high=0.01, size=(
     env.observation_space.n, env.action_space.n))
 
-# lists to store performance metrics
+# Lists to store performance metrics
 total_rewards = []
 total_steps = []
 success_rate = []
@@ -28,32 +33,60 @@ for i in range(num_episodes):
     episode_reward = 0
     num_steps = 0
 
-    # The Q-Table learning algorithm
     while not done:
-        a = np.argmax(Q[s, :] + np.random.randn(1,
-                      env.action_space.n // 1)[0] * (1. / (i + 1)))
-        s_, r, done, _, info = env.step(a)
+        # Epsilon-greedy action selection
+        if np.random.random() < epsilon:  # With probability epsilon, choose a random action
+            action = random.randint(0, 3)
+        else:  # With probability 1-epsilon, choose the action that maximizes the Q-value
+            max_q_value = np.max(Q[s])
+            # Find all actions with the same Q-value as the maximum
+            max_actions = [a for a in range(
+                4) if Q[s, a] == max_q_value]
+            # Choose the action with the smallest index
+            action = min(max_actions)
 
-        # update Q values
-        Q[s, a] = Q[s, a] + lr * (r + gemma * np.max(Q[s_, :]) - Q[s, a])
+        s_, r, done, _, info = env.step(action)
+
+        # Update Q values
+        Q[s, action] = Q[s, action] + lr * \
+            (r + gamma * np.max(Q[s_, :]) - Q[s, action])
         s = s_
-        episode_reward += r
         num_steps += 1
+        episode_reward += r
 
-    # append performance metrics to lists
-    total_rewards.append(episode_reward / num_steps)
+    # Decay epsilon
+    epsilon = epsilon * epsilon_decay
+    lr = 1.0 / (1 + i)
+
+    # Append performance metrics to lists
+    total_rewards.append(episode_reward)
     total_steps.append(num_steps)
     success_rate.append(int(episode_reward > 0))
 
     # Print episode metrics
     print("Episode:", i + 1, "Reward:", episode_reward, "Steps:", num_steps)
 
-# Plotting the average rewards and episode Lengths gained throughout each episode per episode
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-axs[0].plot(total_rewards, 'tab:green')
-axs[0].set_title('Average Reward per Episode')
-axs[1].plot(total_steps, 'tab:purple')
-axs[1].set_title('Number of Steps Taken per Episode')
+plt.plot(total_steps, 'tab:purple')
+plt.title('Number of Steps per Episode')
+plt.xlabel('Episode')
+plt.ylabel('Number of Steps')
+
+plt.show()
+
+# Calculate the sigmoid-transformed rewards
+reward_sigmoid = 1 / (1 + np.exp(-np.array(total_rewards)))
+
+# Plotting
+# Calculate the sigmoid-transformed rewards
+reward_sigmoid = 1 / (1 + np.exp(-np.array(total_rewards)))
+
+# Plotting
+plt.plot(range(1, num_episodes + 1), reward_sigmoid, 'tab:green')
+plt.title('Reward as Sigmoid')
+plt.xlabel('Episode')
+plt.ylabel('Sigmoid(Reward)')
+
+plt.show()
 
 plt.show()
 
